341cf198b0032fc524b168157d99f23bdd4e5b8f
majo84ec <120151872+majo84ec@users.noreply.github.com>
2023-05-14 01:50:38 -0400

    Add files via upload

---
+++ schema_main_table.sql
@@ -0,0 +1,50 @@
+drop table main;
+
+CREATE TABLE main(
+    country_name varchar,
+    country_code varchar primary key,
+    Series_name varchar,
+    Series_code varchar,
+	YR_1990 float,
+    YR_1991 float,
+    YR_1992 float,
+    YR_1993 float,
+    YR_1994 float,
+    YR_1995 float,
+    YR_1996 float,
+    YR_1997 float,
+    YR_1998 float,
+    YR_1999 float,
+    YR_2000 float,
+    YR_2001 float,
+	YR_2002 float,
+    YR_2003 float,
+    YR_2004 float,
+    YR_2005 float,
+    YR_2006 float,
+    YR_2007 float,
+    YR_2008 float,
+    YR_2009 float,
+    YR_2010 float,
+    YR_2011 float,
+    YR_2012 float,
+    YR_2013 float,
+    YR_2014 float,
+    YR_2015 float,
+    YR_2016 float,
+    YR_2017 float,
+    YR_2018 float,
+    YR_2019 float,
+    YR_2020 float,
+    YR_2021 float,
+    YR_2022 float,
+    YR_2023 float,
+    YR_2024 float,
+    YR_2025 float,
+    YR_2026 float
+	);
+	
+	select* from main;
+
+
+	
